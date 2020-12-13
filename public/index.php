0eefffc8dc1766b1d448e7e144600aa9cb6b6354
Alan Conceição <alan.conceicao@jumia.com>
2020-12-13 03:42:50 +0000

    Adding the domain repo in the composer.json

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Pokerphace\Domain\Number;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -13,9 +14,11 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("ei");
+$app->get('/', function (Request $request, Response $response) {
+    $number = new Number(1);
+
+    $response->getBody()->write("ei: " . $number->getNumber());
+
     return $response;
 });
 
